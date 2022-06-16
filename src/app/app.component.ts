13382786dbaea366e8c21fce7c7ff8fe1557aec6
Shakouri Javad <jashakouri@gmail.com>
2022-06-16 13:36:50 +0430

    attribute binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  username = '';
-  password = '';
-  rePassword = '';
-  isMatchPassword = false;
+export class AppComponent implements OnInit {
 
-  public onUserNameInputChanged(e: Event) {
-    this.username = (<HTMLInputElement> e.target).value;
+  isCurrent: boolean = true;
+
+  styles = {};
+  classes = {};
+
+  ngOnInit(): void {
+    this.styles = {
+      'font-size': this.isCurrent ? '4rem' : '2rem',
+      'color': 'red'
+    };
+
+    this.classes = {
+      'big-font-size': this.isCurrent,
+      'cancel-color': this.isCurrent
+    };
+
   }
 
-  public onPasswordInputChanged(e: Event) {
-    this.password = (<HTMLInputElement> e.target).value;
-  }
-
-  public onRePasswordInputChanged(e: Event) {
-    this.rePassword = (<HTMLInputElement> e.target).value;
-    this.isMatchPassword = this.rePassword !== this.password ? false : true;
+  public onClick(e: Event){
+    this.isCurrent = !this.isCurrent;
+    this.classes = {
+      'big-font-size': this.isCurrent,
+      'cancel-color': this.isCurrent
+    };
   }
 
 }
